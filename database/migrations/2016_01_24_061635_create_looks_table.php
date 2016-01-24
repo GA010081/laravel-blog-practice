dac8433dd6fe40467aa29a5d67fb97b8fab391d3
YanMingWu <40143109@gm.nfu.edu.tw>
2016-01-24 15:12:57 +0800

    day3 finished

--- database/migrations/2016_01_24_061635_create_looks_table.php
+++ database/migrations/2016_01_24_061635_create_looks_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateArticlesTable extends Migration
+class CreateLooksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,9 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
+        Schema::create('looks', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title');
-            $table->text('content');
-            $table->text('picture');
             $table->timestamps();
-
         });
     }
 
@@ -29,6 +25,6 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
-        Schema::drop('articles');
+        Schema::drop('looks');
     }
 }
